0cd4a7a0be6e7e5197dd25832d689b6019e1638b
Evan Maloney <evanpmaloney@gmail.com>
2021-11-29 11:06:52 -0800

    deploy to github

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 //------------------BLACKJACK BRAINIAC---------------//
 /*----- constants -----*/
-// const players = {
-//     dealer:
-//         player
-// }
+const dealer = {
+    hand: []
+}
+const player = {
+    hand: []
+}
 
 // if (hand > 21) {
 //     bust
@@ -14,47 +16,33 @@ const values = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 
 const orderedDeck = buildOrderedDeck();
 
 /*----- app's state (variables) -----*/
-let dealerHand, playerHand, readyDeck, shuffledDeckArr, dealtCards
+let readyDeck, shuffledDeckArr, dealtCards
 
 /*----- cached element references -----*/
 const dealerCards = document.getElementById('dealerSpot');
 const playerCards = document.getElementById('playerSpot');
+const dealBtn = document.getElementById('deal')
+const hitBtn = document.getElementById('hit')
+const standBtn = document.getElementById('stand')
+const restartBtn = document.getElementById('restart')
+const message = document.getElementById('messageBox')
+let firstCard = document.getElementById('playerCard1');
+let secondCard = document.getElementById('dealerCard1');
+let thirdCard = document.getElementById('playerCard2');
+let fourthCard = document.getElementById('dealerCard2');
 
 /*----- event listeners -----*/
-const deal = document.getElementById('deal').addEventListener('click', () => {
-    dealtCards = [];
-    dealtCards.push(readyDeck.splice(0, 4));
-    let card1 = dealtCards[0][0].face;
-    let card2 = dealtCards[0][1].face;
-    let card3 = dealtCards[0][2].face;
-    let card4 = dealtCards[0][3].face;
-    let firstCard = document.getElementById('playerCard1');
-    let secondCard = document.getElementById('dealerCard1');
-    let thirdCard = document.getElementById('playerCard2');
-    let fourthCard = document.getElementById('dealerCard2');
-    firstCard.classList.add("card", card1);
-    secondCard.classList.add("card", "back");
-    thirdCard.classList.add("card", card3);
-    fourthCard.classList.add("card", card4);
-
-});
-
-document.getElementById('hit').addEventListener('click', () => {
-    // let card5 = dealtCards[0][5].face;
-});
-
-document.getElementById('stand').addEventListener('click', () => {
-
-});
-
-document.getElementById('restart').addEventListener('click', () => {
-    init();
-});
+dealBtn.addEventListener('click', deal)
+hitBtn.addEventListener('click', hit)
+standBtn.addEventListener('click', stand)
+restartBtn.addEventListener('click', init)
 
 /*----- functions -----*/
 function init() {
-    document.getElementById('messageBox').remove();
+    // message.remove();
     readyDeck = getShuffledDeck();
+    // dealtCards = [];
+    // dealtCards.push(readyDeck.splice(0, 10));
     render();
 }
 
@@ -64,7 +52,7 @@ function buildOrderedDeck() {
         values.forEach(function (value) {
             builtDeck.push({
                 face: `${suit}${value}`,
-                numberedValue: Number(value) || (value === 'A' ? 11 : 10)
+                numberValue: Number(value) || (value === 'A' ? 11 : 10)
             })
         })
     })
@@ -81,8 +69,61 @@ function getShuffledDeck() {
     return shuffledDeckArr;
 }
 
-function render() {
+function deal() {
+    // Deal function should update the player and dealers hands with
+    // the card objects in an array. The render function will check
+    // this array and update the DOM
+    player.hand.push(readyDeck.splice(0, 10)[0])
+    // dealer.hand.push(readyDeck.splice(0, 2)[0])
+    // message.append()
+    checkWin();
+    render();
+}
 
+function hit() {
+    // hit func should just add one more card to the players hand
+    // in state and then immediately check if the player busted
+    // Render function will check the state of the player's hand and
+    // update DOM
+    let hitCard = dealtCards[0][i].face;
+    let card6 = dealtCards[0][5].face;
+    let card7 = dealtCards[0][6].face;
+    let fifthCard = document.getElementById('playerCard3');
+    let sixthCard = document.getElementById('playerCard4');
+    let seventhCard = document.getElementById('playerCard5');
+    fifthCard.classList.add("card", hitCard);
+    sixthCard.classList.add("card", card6);
+    seventhCard.classList.add("card", card7);
+}
+
+function stand() {
+    // Initiate dealer's turn (check if he's at 17, if not keep hitting)
+    // Once dealer is done, compare scores, update if win, tie, or loss
+    // Render function will check the state of dealer's hand as well
+    // as state of winner and render appropriate cards and message to
+    // the DOM
+    let card2 = dealtCards[0][1].face;
+    let secondCard = document.getElementById('dealerCard1');
+    secondCard.classList.replace("back", card2);
+}
+
+function checkWin() {
+    // instead of updating DOM, update the state of the winner based on
+    // the state of the dealer's hand and player's hand
+    //     if (pHand === 21) {
+    //         message.innerText = "You hit Blackjack! You won!!!"
+    //     } else {
+    //         message.innerText = "You have not hit Blackjack. You have yet to win."
+    //     }
+}
+
+function render() {
+    // check the dealers hand and players hand and render cards
+    // check if there's a winner and render appropriate message
+    // firstCard.classList.add("card", card1);
+    // secondCard.classList.add("card", "back");
+    // thirdCard.classList.add("card", card3);
+    // fourthCard.classList.add("card", card4);
 }
 
 init();
